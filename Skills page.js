32de12e18f30b6e82e18a071c75f3a620ecb0c11
Guineapig123456 <121705875+Guineapig123456@users.noreply.github.com>
2024-06-02 17:14:02 +0530

    Add files via upload

---
+++ Skills page.js
@@ -0,0 +1,13 @@
+function Skills(){
+    return(
+        <Section>
+        <h1>Project: </h1>
+        <span> 
+         1. Programming Languages:C,Java
+         2. Data Analytics tools: Tableau,PowerBI
+        3.IOT and Machine Learning(basics) 
+        
+        </span>
+       </Section>
+    );
+}
