84d0762c10aa6aa797613f0bb48136af409c8b99
Guineapig123456 <121705875+Guineapig123456@users.noreply.github.com>
2024-06-05 20:52:29 +0530

    Add files via upload

---
+++ home.1.js
@@ -0,0 +1,31 @@
+ function home(){
+    return(
+        <Section>
+            <aside>
+                <img src ="th.bing.com/th/id/OIP.AnjX-GJRZxpb_hI5NnNewgHaMm?rs=1&pid=ImgDetMain"/>
+        
+        </aside>
+            <header>
+            <h1>Welcome to my Portfolio</h1>
+            <nav>
+                <a href = '#'> Home</a>
+                <a href ='#'Education>Education</a>
+                <a href ='#'> Portfolio</a>
+            </nav>
+        </header>
+        
+        <span> Name:Tanisha Saha</span>
+        
+        <span>Contact no: 8584885563</span>
+        
+        <span>Email id: tanishasaha597@gmail.com</span>
+        
+        <p>  As a 3rd Year B.Tech student majoring in CSE,I seek internship opportunities to apply and enhance my technical skills
+            in my area of interest.I am eager to contribute my passion for Front-End web development and strong academic foundation to a dynamic
+            foundation </p>;
+    </Section >
+    );
+}
+    
+
+
